3920bce4f3754af5acacf51fb97a5b365871b309
buri priyanka <buripriyanka1816@gmail.com>
2023-11-01 11:52:23 +0530

    noraml changes

--- src/components/feedBack.js
+++ src/components/feedBack.js
@@ -31,11 +31,11 @@ console.log(feedbackDetails);
 
   return (
     <div className="feedback-container">
-      <h2>Response Box</h2>
+      <h2>Tell us if our delivery service meets your needs , please share your needs and preferences with us so that we can better serve you?</h2>
       <form onSubmit={handleSubmit}>
         <textarea
           className="feedback-input"
-          placeholder="Tell us if our delivery service meets your needs , please share your needs and preferences with us so that we can better serve you?.....type here and click submit or contact 8121784826"
+          placeholder="type here and click submit or contact 8121784826 ......"
           value={feedback}
           onChange={handleFeedbackChange}
         />
